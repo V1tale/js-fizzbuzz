e55e11fdc13cb4d5dad108378534c41fa04affad
V1tale <elativ.c@gmail.com>
2022-10-10 16:44:57 +0200

    completato

--- js/script.js
+++ js/script.js
@@ -3,19 +3,19 @@
 // Ogni multiplo di 5 prevederà un output pari a "buzz"
 // Ogni multiplo di 3 e di 5, prevederà un output pari a "flizzBuzz"
 
-const row = document.querySelector(".row");
 // LOOP
 for (let i = 1; i <= 100; i++) {
     let fizzBuzz = i ;
     if (i % 3 === 0) 
-      fizzBuzz = "fizz";
+    fizzBuzz = "fizz";
     if (i % 5 === 0)
-      fizzBuzz = "buzz"
+    fizzBuzz = "buzz"
     if (i % 5 === 0 & i % 3 === 0)
     fizzBuzz = "fizzBuzz";
     console.log(fizzBuzz)
     
-// CREATE ELEMENT
+    // OUTPUT
+const row = document.querySelector(".row");
 const cell = document.createElement("div")
 cell.classList.add(fizzBuzz);
 cell.innerHTML = fizzBuzz;
