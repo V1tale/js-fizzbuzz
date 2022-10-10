498628620a5008888c1e4367ee6c9c4aa2a6cfcf
V1tale <elativ.c@gmail.com>
2022-10-10 15:35:33 +0200

    completato js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@
 //  Crea un container nel DOM , aggiungendo (attraverso la funzione `append()`) un elemento html con il numero o la stringa corretta da mostrare. **BONUS 2:**
 //  Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
 //  Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
+for (let i = 1; i <= 100; i++) {
+    let fizzBuzz = i ;
+    if (i % 3 === 0) 
+      fizzBuzz = "fizz";
+    if (i % 5 === 0)
+      fizzBuzz = "buzz"
+    if (i % 5 === 0 & i % 3 === 0)
+    fizzBuzz = "fizzBuzz";
+    console.log(fizzBuzz)
+    }
