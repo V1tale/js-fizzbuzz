cb5b7f25aee7ab77aae659d6bee47853453d7c76
V1tale <elativ.c@gmail.com>
2022-10-10 16:01:54 +0200

    creati gli elementi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@
 // Ogni multiplo di 3 prevederà un output pari a "fizz"
 // Ogni multiplo di 5 prevederà un output pari a "buzz"
 // Ogni multiplo di 3 e di 5, prevederà un output pari a "flizzBuzz"
+
+const row = document.querySelector(".row");
+// LOOP
 for (let i = 1; i <= 100; i++) {
     let fizzBuzz = i ;
     if (i % 3 === 0) 
@@ -11,4 +14,10 @@ for (let i = 1; i <= 100; i++) {
     if (i % 5 === 0 & i % 3 === 0)
     fizzBuzz = "fizzBuzz";
     console.log(fizzBuzz)
-    }
+    
+// CREATE ELEMENT
+const cell = document.createElement("div")
+cell.classList.add(fizzBuzz);
+cell.innerHTML = fizzBuzz;
+row.append(cell);
+}
